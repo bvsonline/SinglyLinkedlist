c168cebc0786acbf224284dfb3c3a00f57149738
Venkata B <bvsonline@gmail.com>
2019-04-24 21:40:27 -0700

    Initial Commit

---
+++ SinglyLinkedlist/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  SinglyLinkedlist
+//
+//  Created by venkata bethamcharla on 4/24/19.
+//  Copyright © 2019 VENKATA. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
